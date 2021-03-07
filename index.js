6ba605b2e4f0f451fa81b4415e7947fd35dbbdb2
KongTM <rhdxoals@gmail.com>
2021-03-07 16:18:27 +0900

    ejs example

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const express = require('express');
 const app = express();
 let PORT = 3000;
 
+const datas = ['kong','goni','laal'];
+
+// ejs part (views folder)
+app.set('views',__dirname + '/views')
+app.set('view engine','ejs');
+
 app.get('/',(req,res)=>{
-    res.send('hello');
-    
+    res.render('hello',{datas:datas});
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login',{datas:'kong'});
+})
+
+
+
+// express part (public folder)
+app.use(express.static(__dirname+'/public'));
+
+app.get('/home',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html');
 })
 
 app.listen(PORT,()=>{
-    console.log('sever is running... port : '+PORT);
+    console.log('sever is running... port : '+ PORT);
 })
